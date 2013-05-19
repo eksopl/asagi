5f9e344478ccf0da76f5daebd87e9e7d6a2e6b19
Hung Tran <oohnoitz@gmail.com>
2013-05-19 16:57:32 -0500

    updated mysql/pgsql schema

--- src/main/resources/net/easymodo/asagi/sql/Pgsql/boards.sql
+++ src/main/resources/net/easymodo/asagi/sql/Pgsql/boards.sql
@@ -5,14 +5,21 @@ CREATE TABLE %%BOARD%%_threads (
   time_bump integer NOT NULL,
   time_ghost integer DEFAULT NULL,
   time_ghost_bump integer DEFAULT NULL,
+  time_last_modified integer NOT NULL,
   nreplies integer NOT NULL DEFAULT '0',
   nimages integer NOT NULL DEFAULT '0',
+  sticky boolean DEFAULT false NOT NULL,
+  locked boolean DEFAULT false NOT NULL,
+
   PRIMARY KEY (thread_num)
 );
 
 CREATE INDEX %%BOARD%%_threads_time_op_index on %%BOARD%%_threads (time_op);
-CREATE INDEX %%BOARD%%_threads_time_bump_index on %%BOARD%%_threads (time_bump);					
+CREATE INDEX %%BOARD%%_threads_time_bump_index on %%BOARD%%_threads (time_bump);
 CREATE INDEX %%BOARD%%_threads_time_ghost_bump_index on %%BOARD%%_threads (time_ghost_bump);
+CREATE INDEX %%BOARD%%_threads_time_last_modified_index on %%BOARD%%_threads (time_last_modified);
+CREATE INDEX %%BOARD%%_threads_sticky_index on %%BOARD%%_threads (sticky);
+CREATE INDEX %%BOARD%%_threads_locked_index on %%BOARD%%_threads (locked);
 
 CREATE TABLE %%BOARD%%_users (
   user_id SERIAL NOT NULL,
@@ -20,7 +27,7 @@ CREATE TABLE %%BOARD%%_users (
   trip character varying(25) NOT NULL DEFAULT '',
   firstseen integer NOT NULL,
   postcount integer NOT NULL,
-  
+
   PRIMARY KEY (user_id),
   UNIQUE (name, trip)
 );
@@ -36,12 +43,13 @@ CREATE TABLE %%BOARD%%_images (
   preview_reply character varying(20),
   total integer NOT NULL DEFAULT '0',
   banned smallint NOT NULL DEFAULT '0',
-  
+
   PRIMARY KEY (media_id),
   UNIQUE (media_hash)
 );
 
 CREATE INDEX %%BOARD%%_images_total_index on %%BOARD%%_images (total);
+CREATE INDEX %%BOARD%%_images_banned_index ON %%BOARD%%_images (banned);
 
 CREATE TABLE %%BOARD%%_daily (
   day integer NOT NULL,
@@ -51,7 +59,7 @@ CREATE TABLE %%BOARD%%_daily (
   anons integer NOT NULL,
   trips integer NOT NULL,
   names integer NOT NULL,
-  
+
   PRIMARY KEY (day)
 );
 
@@ -93,17 +101,15 @@ CREATE TABLE %%BOARD%% (
   UNIQUE (num, subnum)
 );
 
-CREATE INDEX %%BOARD%%_poster_ip_index on %%BOARD%% (poster_ip);
 CREATE INDEX %%BOARD%%_num_index on %%BOARD%% (num);
 CREATE INDEX %%BOARD%%_subnum_index on %%BOARD%% (subnum);
 CREATE INDEX %%BOARD%%_thread_num_index on %%BOARD%% (thread_num);
 CREATE INDEX %%BOARD%%_op_index on %%BOARD%% (op);
-CREATE INDEX %%BOARD%%_timestamp_index on %%BOARD%% (timestamp);
-CREATE INDEX %%BOARD%%_timestamp_expired_index on %%BOARD%% (timestamp_expired);
-CREATE INDEX %%BOARD%%_orig_filename_index on %%BOARD%% (media_orig);
 CREATE INDEX %%BOARD%%_media_hash_index on %%BOARD%% (media_hash) ;
-CREATE INDEX %%BOARD%%_media_filename_index on %%BOARD%% (media_filename) ;
-CREATE INDEX %%BOARD%%_email_index on %%BOARD%% (email);
+CREATE INDEX %%BOARD%%_media_orig_index on %%BOARD%% (media_orig) ;
+CREATE INDEX %%BOARD%%_name_trip_index on %%BOARD%% (name,trip);
 CREATE INDEX %%BOARD%%_name_index on %%BOARD%% (name);
 CREATE INDEX %%BOARD%%_trip_index on %%BOARD%% (trip);
-CREATE INDEX %%BOARD%%_fullname_index on %%BOARD%% (name,trip);
+CREATE INDEX %%BOARD%%_email_index on %%BOARD%% (email);
+CREATE INDEX %%BOARD%%_poster_ip_index on %%BOARD%% (poster_ip);
+CREATE INDEX %%BOARD%%_timestamp_index on %%BOARD%% (timestamp);
